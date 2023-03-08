d3ac3a518134800f86ca3fe3db033d0216ac88bf
Amit Pathak <ampatha@akamai.com>
2023-03-08 21:28:19 +0530

    Updated sms patterns

--- main.go
+++ main.go
@@ -42,11 +42,10 @@ var patterns = []*regexp.Regexp{
 	regexp.MustCompile(`(?m)(?P<txtype>\w+) Card no. (?P<account>\w+) INR (?P<amount>[\d,.]+) (?P<date>[\d-]+) (?P<time>[\d:]+) (?P<receiver>[\w\s]+) Avl Lmt INR (?P<balance>[\d,.]+)`),
 
 	// ICICI debit
-	// https://regex101.com/r/8QNj0Q/1
 	// INR 232.42 spent on ICICI Bank Card XX0000 on 04-Mar-23 at ONE97 COMMUNICA. Avl Lmt: INR 1,23,456.28. To dispute,call 18002662/SMS BLOCK 0000 to 9215676766
 	// INR 965.00 spent on ICICI Bank Card XX0000 on 07-Mar-23 at Amazon.in - Gro. Avl Lmt: INR 1,23,456.28. To dispute,call 18002662/SMS BLOCK 0000 to 9215676766
-	// INR 634.00 spent on ICICI Bank Card XX3002 on 06-Mar-23 at Amazon. Avl Lmt: INR 2,48,113.28. To dispute,call 18002662/SMS BLOCK 3002 to 9215676766
-	// INR 662.74 spent on ICICI Bank Card XX3002 on 04-Mar-23 at UBERINDIASYSTEM. Avl Lmt: INR 2,38,513.54. To dispute,call 18002662/SMS BLOCK 3002 to 9215676766
+	// INR 634.00 spent on ICICI Bank Card XX0000 on 06-Mar-23 at Amazon. Avl Lmt: INR 1,23,456.28. To dispute,call 18002662/SMS BLOCK 0000 to 9215676766
+	// INR 662.74 spent on ICICI Bank Card XX0000 on 04-Mar-23 at UBERINDIASYSTEM. Avl Lmt: INR 1,23,456.54. To dispute,call 18002662/SMS BLOCK 0000 to 9215676766
 	regexp.MustCompile(`(?m)INR (?P<amount>[\d,.]+) (?P<txtype>\w+) on ICICI Bank Card (?P<account>\w+) on (?P<date>[\w-]+) at (?P<receiver>[\w\s\-.]+). Avl Lmt: INR (?P<balance>[\d,]+.?\d{2}?)`),
 
 	// Federal bank debit
@@ -66,23 +65,23 @@ var patterns = []*regexp.Regexp{
 	regexp.MustCompile(`(?m)you've (?P<txtype>\w+) INR (?P<amount>[\d,.]+) in your Account (?P<account>\w+). Woohoo! It was sent by (?P<sender>[\w]+) on (?P<date>[\w\s,-]+)`),
 
 	// Citibank debit
-	// Your Citibank A/c has been debited with INR 194.00 on 07-MAR-2023 at 17:22 and account paytmqr28100505010114n4k18nnfrt@paytm has been credited. UPI Ref no. 306655219320
+	// Your Citibank A/c has been debited with INR 194.00 on 07-MAR-2023 at 17:22 and account paytmqr28100505010114n4k18nnfrt@paytm has been credited. UPI Ref no. 30612345219320
 
 	// Citibank ECS debit
-	// We confirm ECS debit on your Citi account no. XXXXXX9228 on 06-MAR-23 for an amount of Rs. 40817
+	// We confirm ECS debit on your Citi account no. XXXXXX1234 on 06-MAR-23 for an amount of Rs. 12345
 
 	// HDFC food card credit
-	// Your HDFC Bank Visa Foodplus Card Card ending with XXXX1879 has been reloaded with INR 1100.00 on 13-JAN-23 03:01 PM. Post Reload Card Bal is INR 1248.67. Not you? Call 18002586161.
+	// Your HDFC Bank Visa Foodplus Card Card ending with XXXX1234 has been reloaded with INR 1100.00 on 13-JAN-23 03:01 PM. Post Reload Card Bal is INR 1234.67. Not you? Call 18002586161.
 
 	// HDFC CC debit
 	// https://regex101.com/r/utoJUX/1
-	// You've spent Rs.5 On HDFC Bank CREDIT Card xx6530 At PHONEPE PRIVATE LTD On 2023-01-10:13:39:47 Avl bal: Rs.684729.15 Curr O/s: Rs.14270.85 Not you?Call 18002586161
-	// You've spent Rs.2200 On HDFC Bank CREDIT Card xx6530 At ST THERESA EDUCATIONAL On 2023-01-10:11:43:59 Avl bal: Rs.684734 Curr O/s: Rs.14266 Not you?Call 18002586161
-	// You've spent Rs.776.33 On HDFC Bank CREDIT Card xx6530 At NHPS CC On 2022-12-30:20:41:47 Avl bal: Rs.680268.24 Curr O/s: Rs.18731.76 Not you?Call 18002586161
-	// You've spent Rs.234.82 On HDFC Bank CREDIT Card xx6530 At BBPSBILLPAY On 2022-12-30:13:46:04 Avl bal: Rs.682566.18 Curr O/s: Rs.16433.82 Not you?Call 18002586161
-	// You've spent Rs.7394 On HDFC Bank CREDIT Card xx6530 At Decathlon On 2022-12-24:17:18:34 Avl bal: Rs.681658 Curr O/s: Rs.17342 Not you?Call 18002586161
-	// You've spent Rs.9947.52 On HDFC Bank CREDIT Card xx6530 At The Paul Bangalore On 2022-12-23:16:13:03 Avl bal: Rs.689052.48 Curr O/s: Rs.9947.52 Not you?Call 18002586161
-	// You've spent Rs.920.08 On HDFC Bank CREDIT Card xx6530 At Kids Clinic In Banglor On 2022-12-18:09:57:08 Avl bal: Rs.697311.92 Curr O/s: Rs.1688.08 Not you?Call 18002586161
+	// You've spent Rs.5 On HDFC Bank CREDIT Card xx0000 At PHONEPE PRIVATE LTD On 2023-01-10:13:39:47 Avl bal: Rs.123456.15 Curr O/s: Rs.14270.85 Not you?Call 18002586161
+	// You've spent Rs.2200 On HDFC Bank CREDIT Card xx0000 At ST THERESA EDUCATIONAL On 2023-01-10:11:43:59 Avl bal: Rs.123456 Curr O/s: Rs.14266 Not you?Call 18002586161
+	// You've spent Rs.776.33 On HDFC Bank CREDIT Card xx0000 At NHPS CC On 2022-12-30:20:41:47 Avl bal: Rs.123456.24 Curr O/s: Rs.18731.76 Not you?Call 18002586161
+	// You've spent Rs.234.82 On HDFC Bank CREDIT Card xx0000 At BBPSBILLPAY On 2022-12-30:13:46:04 Avl bal: Rs.123456.18 Curr O/s: Rs.16433.82 Not you?Call 18002586161
+	// You've spent Rs.7394 On HDFC Bank CREDIT Card xx0000 At Decathlon On 2022-12-24:17:18:34 Avl bal: Rs.123456 Curr O/s: Rs.17342 Not you?Call 18002586161
+	// You've spent Rs.9947.52 On HDFC Bank CREDIT Card xx0000 At The Paul Bangalore On 2022-12-23:16:13:03 Avl bal: Rs.123456.48 Curr O/s: Rs.9947.52 Not you?Call 18002586161
+	// You've spent Rs.920.08 On HDFC Bank CREDIT Card xx0000 At Kids Clinic In Banglor On 2022-12-18:09:57:08 Avl bal: Rs.123456.92 Curr O/s: Rs.1688.08 Not you?Call 18002586161
 	regexp.MustCompile(`(?m)You've (?P<txtype>\w+) Rs.(?P<amount>[\d,.]+) On HDFC Bank CREDIT Card (?P<account>\w+) At (?P<receiver>[\w\s.]+) On (?P<date>[\d-]+):(?P<time>[\d:]+) Avl bal: Rs.(?P<balance>[\d,.]+)`),
 }
 
@@ -105,24 +104,13 @@ var dateRe = regexp.MustCompile(`(?m)(\d{2}-\w{3}-\d{2})|(\d{2}-\d{2}-\d{4})|(\d
 var timeRe = regexp.MustCompile(`(?m)\d{2}:\d{2}`)
 
 func main() {
-
-	var err error
-	// Custom log file configuration.
-	//fileName := "mtrack.log"
-	//logFile, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-	//defer logFile.Close()
-	//wrt := io.MultiWriter(logFile)
-	//log.SetOutput(wrt)
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
 	router := gin.Default()
 	router.GET("v1/messages", getMessages)
 	router.POST("v1/messages", postMessage)
 
-	err = router.Run("localhost:8080")
+	err := router.Run("localhost:8080")
 	if err != nil {
 		panic(err)
 	}
@@ -264,7 +252,6 @@ func postMessage(c *gin.Context) {
 
 	balanceStr := tMap["balance"]
 	if balanceStr == "" {
-		//log.Println("Balance amount not found in SMS ->", smsText)
 		balanceStr = "0"
 	}
 	balance, err := parseAmount(balanceStr)
