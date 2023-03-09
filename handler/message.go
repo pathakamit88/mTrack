6b03a7d72eb1d33b59ca5e61705b28cdc9141b79
Amit Pathak <ampatha@akamai.com>
2023-03-09 17:23:53 +0530

    Added handler module

---
+++ handler/message.go
@@ -0,0 +1,122 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
+type sms struct {
+	M string `json:"m"`
+}
+
+type message struct {
+	TxType   string    `json:"type"`
+	Bank     string    `json:"bank"`
+	Account  string    `json:"account"`
+	Amount   float64   `json:"amount"`
+	Sender   string    `json:"sender"`
+	Receiver string    `json:"receiver"`
+	Balance  float64   `json:"balance"`
+	DateTime time.Time `json:"time"`
+}
+
+var messages []message
+
+func GetMessages(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, messages)
+}
+
+func PostMessage(c *gin.Context) {
+	var newSMS sms
+	var msg message
+	var err error
+
+	if err := c.BindJSON(&newSMS); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	smsText := newSMS.M
+
+	bankName := bankRe.FindString(smsText)
+	if bankName == "" {
+		log.Println("Bank name not found in SMS ->"+
+			"", smsText)
+		c.String(http.StatusOK, "Bank name not found")
+		return
+	}
+
+	var tMap = map[string]string{
+		"amount":   "",
+		"txtype":   "",
+		"account":  "",
+		"date":     "",
+		"time":     "",
+		"receiver": "",
+		"balance":  "",
+	}
+	for _, p := range patterns {
+		matches := p.FindStringSubmatch(smsText)
+		if matches != nil {
+			names := p.SubexpNames()
+			for i, match := range matches {
+				if i != 0 {
+					tMap[names[i]] = match
+				}
+			}
+			break
+		}
+	}
+
+	txTypeStr := tMap["txtype"]
+	if txTypeStr == "" {
+		log.Println("Transaction type not found in SMS ->", smsText)
+		c.String(http.StatusOK, "Transaction type not found")
+		return
+	}
+	txType := parseTransactionType(txTypeStr)
+	if txType == "" {
+		log.Println("Transaction type parsing error ->", txTypeStr)
+		c.String(http.StatusInternalServerError, "Transaction type parsing error")
+		return
+	}
+
+	amountStr := tMap["amount"]
+	if amountStr == "" {
+		log.Println("Transaction amount not found in SMS ->", smsText)
+		amountStr = "0"
+	}
+	amount, err := parseAmount(amountStr)
+	if err != nil {
+		log.Println("Amount parse error ->", amountStr, smsText)
+		c.String(http.StatusInternalServerError, "Amount parsing error")
+		return
+	}
+
+	txDate, err := parseDateTime(tMap["date"], tMap["time"])
+	if err != nil {
+		log.Println("Date parse error ->", tMap["date"], tMap["time"], err)
+	}
+
+	balanceStr := tMap["balance"]
+	if balanceStr == "" {
+		balanceStr = "0"
+	}
+	balance, err := parseAmount(balanceStr)
+	if err != nil {
+		log.Println("Balance amount parse error ->", balanceStr)
+		c.String(http.StatusInternalServerError, "Balance amount parsing error")
+		return
+	}
+
+	msg.Amount = amount
+	msg.TxType = txType
+	msg.Bank = bankName
+	msg.Account = tMap["account"]
+	msg.DateTime = txDate
+	msg.Receiver = tMap["receiver"]
+	msg.Balance = balance
+
+	c.IndentedJSON(http.StatusCreated, msg)
+}
---
+++ handler/parse.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const txDebit = "Debit"
+const txCredit = "Credit"
+
+func parseAmount(m string) (float64, error) {
+	amountStr := strings.Replace(m, ",", "", -1)
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("parseAmount error: %v", err)
+	}
+	return amount, nil
+}
+
+func parseDateTime(dt string, tm string) (time.Time, error) {
+	var t time.Time
+	var err error
+	loc, _ := time.LoadLocation("Asia/Kolkata")
+
+	dateStr := dateRe.FindString(dt)
+	if dateStr == "" {
+		return t, errors.New("no date string found")
+	}
+	tStr := timeRe.FindString(tm)
+	if tStr == "" {
+		tStr = time.Now().In(loc).Format("15:04")
+	}
+
+	dateStr = dateStr + " " + tStr
+	var dateLayout = []string{
+		"02-Jan-06 15:04",
+		"02-01-2006 15:04",
+		"2006-01-02 15:04",
+		"02/01/06 15:04",
+		"02-Jan-2006 15:04",
+		"02-01-06 15:04",
+		"January 2, 2006 15:04",
+	}
+	for _, l := range dateLayout {
+		t, err = time.ParseInLocation(l, dateStr, loc)
+		if err == nil {
+			break
+		}
+
+	}
+	if err != nil {
+		return time.Now().In(loc), err
+	}
+	return t, nil
+}
+
+func parseTransactionType(t string) string {
+	var txType string
+	_, exist := debitStrMap[t]
+	if exist != false {
+		txType = txDebit
+	}
+	_, exist = creditStrMap[t]
+	if exist != false {
+		txType = txCredit
+	}
+	return txType
+}
